0028ae1522b7f921f7a5115338e34285ac7155b8
Vinay-s-menase <msvinay456@gmail.com>
2025-09-10 12:50:51 +0530

    added validation for signin and signup page

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Link } from "react-router-dom";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" to="/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
+  "@global": {
     a: {
-      textDecoration: 'none',
+      textDecoration: "none",
     },
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -52,6 +52,42 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn({ onSignIn }) {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({});
+
+  const handleError = () => {
+    setError({});
+    let errorPresent = false;
+    const regexExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!regexExp.test(email)) {
+      setError((prev) => ({ ...prev, email: "Enter email properly" }));
+      errorPresent = true;
+    }
+    if (password.length < 4) {
+      setError((prev) => ({
+        ...prev,
+        password: "Enter password of atleast 4 characters",
+      }));
+      errorPresent = true;
+    }
+    return errorPresent;
+  };
+
+  const handleSignIn = () => {
+    const errorPresent = handleError();
+
+    if (!errorPresent) {
+      const alertOne = confirm("Are you sure email and password are correct?");
+      if (alertOne) {
+        console.log(email, password);
+        onSignIn && onSignIn();
+        setError({});
+      } else {
+        return;
+      }
+    }
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -77,7 +113,10 @@ export default function SignIn({ onSignIn }) {
             name="email"
             autoComplete="email"
             autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
+          <Typography color="error">{error.email}</Typography>
           <TextField
             variant="outlined"
             margin="normal"
@@ -88,7 +127,10 @@ export default function SignIn({ onSignIn }) {
             type="password"
             id="password"
             autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
+          <Typography color="error">{error.password}</Typography>
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
@@ -99,7 +141,7 @@ export default function SignIn({ onSignIn }) {
             variant="contained"
             color="primary"
             className={classes.submit}
-            onClick={onSignIn}
+            onClick={handleSignIn}
           >
             Sign In
           </Button>
--- packages/auth/src/components/Signup.js
+++ packages/auth/src/components/Signup.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Link } from "react-router-dom";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link to="/">Your Website</Link> {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
+  "@global": {
     a: {
-      textDecoration: 'none',
+      textDecoration: "none",
     },
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -49,6 +49,60 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp({ onSignIn }) {
   const classes = useStyles();
+  const [userData, setUserData] = useState({});
+  const [error, setError] = useState({});
+
+  const handleError = () => {
+    setError({});
+    let errorPresent = false;
+    const regexExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!regexExp.test(userData?.email)) {
+      setError((prev) => ({ ...prev, email: "Enter email properly" }));
+      errorPresent = true;
+    }
+    if (!userData?.password || userData?.password?.length < 4) {
+      setError((prev) => ({
+        ...prev,
+        password: "Enter password of atleast 4 characters",
+      }));
+      errorPresent = true;
+    }
+    if (!userData.firstName || userData.firstName.length <= 0) {
+      setError((prev) => ({
+        ...prev,
+        firstName: "Enter First name",
+      }));
+      errorPresent = true;
+    }
+    if (!userData.lastName || userData.lastName.length <= 0) {
+      setError((prev) => ({
+        ...prev,
+        lastName: "Enter Last name",
+      }));
+      errorPresent = true;
+    }
+    return errorPresent;
+  };
+
+  const handleSignIn = () => {
+    const errorPresent = handleError();
+
+    if (!errorPresent) {
+      const alertOne = confirm("Are you sure email and password are correct?");
+      if (alertOne) {
+        console.log(userData?.email, userData?.password);
+        onSignIn && onSignIn();
+        setError({});
+      } else {
+        return;
+      }
+    }
+  };
+
+  const handleUserData = (e, key) => {
+    const { value } = e.target;
+    setUserData((prev) => ({ ...prev, [key]: value }));
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -75,7 +129,10 @@ export default function SignUp({ onSignIn }) {
                 id="firstName"
                 label="First Name"
                 autoFocus
+                value={userData?.firstName}
+                onChange={(e) => handleUserData(e, "firstName")}
               />
+              <Typography color="error">{error.firstName}</Typography>
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -86,7 +143,10 @@ export default function SignUp({ onSignIn }) {
                 label="Last Name"
                 name="lastName"
                 autoComplete="lname"
+                value={userData?.lastName}
+                onChange={(e) => handleUserData(e, "lastName")}
               />
+              <Typography color="error">{error.lastName}</Typography>
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -97,7 +157,10 @@ export default function SignUp({ onSignIn }) {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                value={userData?.email}
+                onChange={(e) => handleUserData(e, "email")}
               />
+              <Typography color="error">{error.email}</Typography>
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -109,7 +172,10 @@ export default function SignUp({ onSignIn }) {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                value={userData?.password}
+                onChange={(e) => handleUserData(e, "password")}
               />
+              <Typography color="error">{error.password}</Typography>
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
@@ -124,7 +190,7 @@ export default function SignUp({ onSignIn }) {
             variant="contained"
             color="primary"
             className={classes.submit}
-            onClick={onSignIn}
+            onClick={handleSignIn}
           >
             Sign Up
           </Button>
